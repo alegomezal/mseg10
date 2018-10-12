39d5d1a224f4c7d733a3a29cfe9ad7c800a49203
alegomezal <43553363+alegomezal@users.noreply.github.com>
2018-10-11 22:55:56 -0600

    Lab 1 file upload + Cenfochat
    Lab 1 file upload + Cenfochat

---
+++ Entregables/7-CenfoChat/main.js
@@ -0,0 +1,31 @@
+
+// #1 - npm install para descargar los paquetes...
+
+// libreriuas
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+// root: presentar html
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg,user){
+     socket.username = user;
+     io.emit('chat message2', user + ": " + msg, user);
+   });
+
+});
+
+  // #2 - Generar Eveto de broadcast
+  // ESCUCHAR EVENTO "chat message"
+  // DESPUES VUELVA A EMITIR EL MENSAJE (broadcast)
+
+
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
